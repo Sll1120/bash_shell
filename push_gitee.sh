6e34b25ca0f9335110ed2b1d0e9a1a5b204bbcc7
sll917 <sll917@hotmail.com>
2022-10-08 11:07:12 +0800

    change v1.1.4

--- push_gitee.sh
+++ push_gitee.sh
@@ -3,7 +3,7 @@
 # * Author : liangliangsu
 # * Email : sll917@hotmail.com
 # * Create time : 2022-09-20 16:46
-# * Filename : push_github.sh
+# * Filename : push_gitee.sh
 # **********************************************************
 git add .
 git commit -m "change v1.$1"
