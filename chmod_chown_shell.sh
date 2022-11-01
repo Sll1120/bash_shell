82138d0bd1f3817ca101f699caddcac9ee274c6b
Sll1120 <sll917@hotmail.com>
2022-11-01 11:03:22 +0800

    change V2022-11-01 11:03

--- chmod_chown_shell.sh
+++ chmod_chown_shell.sh
@@ -5,13 +5,13 @@
 # Email:sll917@hotmail.com
 # Date of writing:2022-10-06 01:42
 #======================================
-chmod u+rwx *.sh */*.sh
+chmod -R u+rwx *.sh */*.sh
 chmod u+rwx */ 
 chmod g-wx *.sh && chmod g+r *.sh
-chmod g-wx */*.sh && chmod g+r */*.sh
+chmod -R g-wx */*.sh && chmod g+r */*.sh
 chmod g-wx */ 
 chmod o-wx *.sh && chmod o+r *.sh
-chmod o-wx */*.sh && chmod o+r */*.sh
+chmod -R o-wx */*.sh && chmod o+r */*.sh
 chmod o-wx */ 
-chown sll:sll *.sh && chown sll:sll */*.sh
+chown -R sll:sll *.sh */*.sh
 chown sll:sll */  
