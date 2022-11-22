476b9de8512a2a12d8f8b897865cce464380edf6
sll1120 <sll917@hotmail.com>
2022-11-22 22:37:49 +0800

    Released in 2022-11-22 22:37

--- rename.sh
+++ rename.sh
@@ -10,9 +10,9 @@
 #do
 #	mv ${file}  ` echo $file | sed 's/shell_//g' `   #sed命令进行文件名替换
 #done
-files=`find . -type d -name  "A*"`         #寻找csv类型的文件
+files=`find . -type d -name  "a*"`         #寻找csv类型的文件
 for file in $files                   #遍历files
 do
 	#grep $1 $file | awk -F, '{print $2,$5,$6}'      #将输入参数1与当前文件内容进行匹配
-	mv ${file}  ` echo $file | sed 's/A/a/g' `   #sed命令进行文件名替换
+	mv ${file}  ` echo $file | sed 's/a/A/g' `   #sed命令进行文件名替换
 done
