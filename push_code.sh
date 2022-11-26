151d403e6edcbb657b02c920c57113034eac1bff
sll1120 <sll917@hotmail.com>
2022-11-26 10:53:34 +0800

    Released in 2022-11-26 10:53

--- push_code.sh
+++ push_code.sh
@@ -9,7 +9,8 @@ git add .
 git commit -m "Released in `date "+%Y-%m-%d %H:%M"`"
 #git push github master
 #git push gitee master
-echo "start push code to github"
+echo -e "\033[31m start push code to github.\033[0m"
+sleep 2
 git push -u github +master
-echo "start push code to gitee"
+echo -e "\033[31m start push code to gitee.\033[0m"
 git push -u gitee +master
