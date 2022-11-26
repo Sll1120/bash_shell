0f0192c39ee5f5c69bbefaa70d1d55de032d171d
sll1120 <sll917@hotmail.com>
2022-11-26 10:40:27 +0800

    Released in 2022-11-26 10:40

--- pull_code.sh
+++ pull_code.sh
@@ -3,7 +3,7 @@
 # * Author : liangliangSu
 # * Email : sll917@hotmail.com
 # * Create time : 2022-10-30 22:27
-# * Filename : pull_gitee.sh
+# * Filename : pull_code.sh
 # **********************************************************
 git fetch --all
 git reset --hard github/master
