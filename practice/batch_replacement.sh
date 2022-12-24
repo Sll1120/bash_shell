789fb195bec0b41a1f96d8be1f0d5317dfc7e591
sll1120 <sll917@hotmail.com>
2022-12-24 19:54:42 +0800

    Released in 2022-12-24 19:54

---
+++ practice/batch_replacement.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# **********************************************************
+# * Author : liangliangSu
+# * Email : sll917@hotmail.com
+# * Create time : 2022-12-10 11:26
+# * Filename : batch_replacement.sh
+# **********************************************************
+sed -n '37 s/T3/30/' `grep -r "T3" testFile`
