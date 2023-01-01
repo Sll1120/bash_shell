907853448f13b099d4251f9e98fa1d5cf40e1ea8
sll1120 <sll917@hotmail.com>
2023-01-01 13:38:55 +0800

    Released in 2023-01-01 13:38

--- a17_common_script/36从shell脚本发送邮件.sh
+++ a17_common_script/36从shell脚本发送邮件.sh
@@ -9,9 +9,9 @@
 EMAIL_RECIVER="sll917@outlook.com"
 
 #发送者邮箱
-EMAIL_SENDER=sll917@qq.com
+EMAIL_SENDER="sll917@qq.com"
 #邮箱用户名
-EMAIL_USERNAME=sll917
+EMAIL_USERNAME="sll917"
 #邮箱密码
 #使用qq邮箱进行发送需要注意：首先需要开启：POP3/SMTP服务，其次发送邮件的密码需要使用在开启POP3/SMTP服务时候腾讯提供的第三方客户端登陆码。
 EMAIL_PASSWORD=kyajdhtjeasubiha
@@ -22,7 +22,7 @@ FILE1_PATH="/home/sll/git/shell/a17_common_script/z_editors.txt"
 #smtp服务器地址
 EMAIL_SMTPHOST=smtp.qq.com
 
-EMAIL_TITLE="测试"
-EMAIL_CONTENT="谢谢!"
+subject="Greetings"
+message="Welcome to Ubuntu"
 
-sendEmail -f ${EMAIL_SENDER} -t ${EMAIL_RECIVER} -s ${EMAIL_SMTPHOST} -u ${EMAIL_TITLE} -xu ${EMAIL_USERNAME} -xp ${EMAIL_PASSWORD} -m ${EMAIL_CONTENT} -a ${FILE1_PATH} -o message-charset=utf-8
+sendEmail -f ${EMAIL_SENDER} -t ${EMAIL_RECIVER} -s ${EMAIL_SMTPHOST} -u ${subject} -xu ${EMAIL_USERNAME} -xp ${EMAIL_PASSWORD} -m ${message} -a ${FILE1_PATH} -o message-charset=utf-8
