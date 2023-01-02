dc7038efdebd82e6370fbea354d618ff9a789ca5
sll1120 <sll917@hotmail.com>
2023-01-02 23:29:33 +0800

    Released in 2023-01-02 23:29

--- a16_common_script/01系统升级.sh
+++ a16_common_script/01系统升级.sh
@@ -6,10 +6,8 @@
 # * Filename : 01系统升级.sh 
 # **********************************************************
 echo -e "\n$(date "+%Y-%m-%d-%T") --- 开始工作\n" 
-
-apt-get update
-apt-get -y upgrade
-apt-get -y autoremove
-apt-get autoclean
-
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y autoremove
+sudo apt-get autoclean
 echo -e "\n$(date "+%Y-%m-%d-%T") --- 结束工作\n" 
