dc1be257fe5a41010a2d7977b8656afd567813ca
sll1120 <sll917@hotmail.com>
2023-01-02 23:26:09 +0800

    Released in 2023-01-02 23:26

--- push_code.sh
+++ push_code.sh
@@ -7,14 +7,14 @@
 # **********************************************************
 git add .
 git commit -m "Released in `date "+%Y-%m-%d %H:%M"`"
-echo -e "\033[32m start push code to github.\033[0m"
-sleep 1
-#git push github master
-git push -u github +master
-
-git add .
-git commit -m "Released in `date "+%Y-%m-%d %H:%M"`"
 echo -e "\033[32m start push code to gitee.\033[0m"
 sleep 1
 #git push gitee master
 git push -u gitee +master
+
+git add .
+git commit -m "Released in `date "+%Y-%m-%d %H:%M"`"
+echo -e "\033[32m start push code to github.\033[0m"
+sleep 1
+#git push github master
+git push -u github +master
