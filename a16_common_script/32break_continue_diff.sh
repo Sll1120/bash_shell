42f97b4f094b81e11a3c38752445034ba7020e3d
Sll1120 <sll917@hotmail.com>
2023-02-04 09:24:01 +0800

    Released in 2023-02-04 09:24

--- a16_common_script/32break_continue_diff.sh
+++ a16_common_script/32break_continue_diff.sh
@@ -12,11 +12,11 @@
 echo "(1)-----------------------------------------------"
 #示例 1：在死循环中，满足条件终止循环
 while true; do
-  let N++
-  if [ $N -eq 5 ]; then
-    break
-fi
-  echo -e "$N\n"
+	let N++
+	if [ $N -eq 5 ]; then
+		break
+	fi
+	echo -e "$N\n"
 done
 #输出：1 2 3 4
 
@@ -24,19 +24,19 @@ echo "(2)-----------------------------------------------"
 #示例 2：举例子说明 continue 用法
 N=0
 while [ $N -lt 5 ]; do
-  let N++
-if [ $N -eq 3 ]; then
-  continue
-fi
-  echo -e "$N\n"
+	let N++
+	if [ $N -eq 3 ]; then
+		continue
+	fi
+	echo -e "$N\n"
 done
-#输出：1 2 4
+#输出：1 2 4 5	
 
 echo "(3)-----------------------------------------------"
 #示例 3: 打印 1-20 数字
 i=0
-while ((i<=20))
-do
-	echo  -e "$i"
-        i=`expr $i + 1`
+while ((i <= 20)); do
+	echo -e "$i"
+	i=$(expr $i + 1)
 done
+
