6a53c7136e5ae68661a055acd484a931c842eeb8
sll1120 <sll917@hotmail.com>
2023-02-05 22:20:21 +0800

    Released in 2023-02-05 22:20

--- a01_common_script/22bacth_create_user.sh
+++ a01_common_script/22bacth_create_user.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-#======================================
-# File name:create_user.sh
-# Author:liangliangSu
-# Email:sll917@hotmail.com
-# Date of writing:2022-10-07 19:13
-#======================================
-if [ $USER == "root" ] 
-then 
-seq -w 10|sed -r "s/(.*)/useradd sll\1/g" | bash
-echo sll{01..10}:$((RANDOM))|tr " " "\n" > z_user.log 
-chpasswd < z_user.log 
-else 
- echo "您不是管理员，没有权限安装软件"
-fi
+# **********************************************************
+# * Author : liangliangSu
+# * Email : sll917@hotmail.com
+# * Create time : 2023-02-05 21:40
+# * Filename : 创建10个user.sh
+# **********************************************************
+#创建10个用户，并分别设置密码，密码要求10位且包含大小写字母以及数字
+#最后需要把每个用户的密码存在指定文件中
+#前提条件：安装mkpasswd命令
+
+#生成10个用户的序列（00-09）
+for u in $(seq -w 00 09); do
+	#创建用户
+	sudo useradd user_$u
+	#生成密码
+	p=$(mkpasswd -s 0 )
+	#从标准输入中读取密码进行修改（不安全）
+	echo $p |sudo passwd --stdin user_$u
+	#常规修改密码
+	echo -e "$p\n$p" |sudo passwd user_$u
+	#将创建的用户及对应的密码记录到日志文件中
+	echo "user_$u $p" >>/tmp/userpassword
+done
