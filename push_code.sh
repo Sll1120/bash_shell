5e62e9e10310b0eb57ebafcd8fb7bd88e861e90d
Sll1120 <sll917@hotmail.com>
2023-02-08 15:36:46 +0800

    Released in 2023-02-08 15:36

--- push_code.sh
+++ push_code.sh
@@ -11,7 +11,9 @@ echo -e "\033[32m start push code to gitee.\033[0m"
 sleep 1
 #git push gitee master
 git push -u gitee +master
+echo -e "gitee 服务器推送 success !"
 
 sleep 2
 #git push github master
 git push -u github +master
+echo -e "github 服务器推送 success !"
