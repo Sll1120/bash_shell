887a4852dcb96fba10f48aa09505359984e92b9b
sll1120 <sll917@hotmail.com>
2023-02-11 16:14:32 +0800

    Released in 2023-02-11 16:14

--- push_code.sh
+++ push_code.sh
@@ -13,9 +13,9 @@ sleep 1
 #git push gitee master
 git push -u gitee +master
 if [ $? = 0 ];then
-	echo -e "\033[1;32m gitee 服务器推送 success ! \033[0m"
+	echo -e "\e[1;32m gitee 服务器推送 success ! \e[0m"
 else
-	echo -e "\033[1;31m gitee 服务器推送 fail ! \033[0m"
+	echo -e "\e[1;31m gitee 服务器推送 fail ! \e[0m"
 fi
 
 sleep 1
