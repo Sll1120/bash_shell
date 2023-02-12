729258438887d4d7b60e9250838847bf8b9c55ae
sll1120 <sll917@hotmail.com>
2023-02-12 21:33:55 +0800

    Released in 2023-02-12 21:33

--- push_code.sh
+++ push_code.sh
@@ -7,22 +7,22 @@
 # **********************************************************
 git add .
 git commit -m "Released in `date "+%Y-%m-%d %H:%M"`"
-echo -e "\033[32m start push code to gitee.\033[0m"
+echo -e "\033[32m start push code to server!\033[0m"
 sleep 1
 
 #git push gitee master
 git push -u gitee +master
 if [ $? = 0 ];then
-	echo -e "\e[1;32m gitee 服务器推送 success ! \e[0m"
+	echo -e "\e[1;32m gitee Server push success ! \e[0m"
 else
-	echo -e "\e[1;31m gitee 服务器推送 fail ! \e[0m"
+	echo -e "\e[1;31m gitee Server push fail ! \e[0m"
 fi
 
 echo -e "-------------------------------------------------"
 #git push github master
 git push -u github +master
 if [ $? = 0 ];then
-	echo -e "\033[1;32m github 服务器推送 success ! \033[0m"
+	echo -e "\033[1;32m github Server push success ! \033[0m"
 else
-	echo -e "\033[1;31m github 服务器推送 fail ! \033[0m"
+	echo -e "\033[1;31m github Server push fail ! \033[0m"
 fi
