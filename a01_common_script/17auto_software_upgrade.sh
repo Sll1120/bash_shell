1eace6b3c51cdc1bcda0683b7d24dad98cff76c8
sll1120 <sll917@hotmail.com>
2023-02-25 09:34:45 +0800

    Released in 2023-02-25 09:34

--- a01_common_script/17auto_software_upgrade.sh
+++ a01_common_script/17auto_software_upgrade.sh
@@ -6,11 +6,17 @@
 # * Filename : 01系统升级.sh 
 # **********************************************************
 echo -e "\n$(date "+%Y-%m-%d-%T") --- 开始工作\n" 
-sudo apt-get remove thunderbird simple-scan simple-scan gnome-mahjongg aisleriot gnome-mines cheese transmission-common gnome-orca gnome-sudoku  totem rhythmbox onboard deja-dup libreoffice-common
-sudo apt-get install vim vpnc git axel openssh-server cmake qtcreator build-essential gdb axel lnav unrar net-tools terminator
+
+sudo apt-get remove thunderbird simple-scan simple-scan 
+\gnome-mahjongg aisleriot gnome-mines cheese transmission-common 
+\gnome-orca gnome-sudoku  totem rhythmbox onboard deja-dup libreoffice-common
+
+sudo apt-get install vim vpnc git axel openssh-server 
+\cmake qtcreator build-essential gdb axel lnav unrar net-tools terminator
 
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get -y autoremove
 sudo apt-get autoclean
+
 echo -e "\n$(date "+%Y-%m-%d-%T") --- 结束工作\n" 
